7a680cdff4298740dcb116d7020ecc4cfa6693c6
Mehmet Nafi OLUR <olurmehmetnafi@gmail.com>
2021-11-19 15:30:19 +0300

    Fikstür Ekleme

---
+++ Controllers/FiksturController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlpataAmatörKüme.Controllers
+{
+    public class FiksturController : Controller
+    {
+        // GET: Fikstur
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FiksturEkle()
+        {
+            return View();
+        }
+    }
+}
