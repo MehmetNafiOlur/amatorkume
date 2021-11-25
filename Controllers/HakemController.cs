ef7b4aecd3a468863cae0f1ddca058cb0e153c74
Mehmet Nafi OLUR <olurmehmetnafi@gmail.com>
2021-11-25 18:25:22 +0300

    YeniGüncellemeler

---
+++ Controllers/HakemController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlpataAmatörKüme.Controllers
+{
+    public class HakemController : Controller
+    {
+      
+        HakemManager hm = new HakemManager(new EfHakemDal());
+        
+        // GET: Hakem
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddHakem()
+        {
+            var q = hm.GetList();
+            return View();
+        
+        }
+
+        public ActionResult AddHakem(Hakem p)
+        {
+            hm.HakemAdd(p);
+            return RedirectToAction("AddHakem");  
+
+        }
+
+    }
+}
---
+++ Controllers/OyuncuBilgiController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlpataAmatörKüme.Controllers
+{
+    public class OyuncuBilgiController : Controller
+    {
+
+        OyuncuBilgiManager ob = new OyuncuBilgiManager(new EfOyuncuBilgiDal());
+        // GET: OyuncuBilgi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddOyuncuBilgi()
+        {
+            var q = ob.GetList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddOyuncuBilgi(OyuncuBilgi p)
+        {
+            ob.OyuncuBilgiAdd(p);
+            return RedirectToAction("AddOyuncuBilgi");
+           
+        }
+    }
+}
